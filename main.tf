24b1cbe59c2369fdb53dff1dfd150625116857e8
sruthi012003 <121019442+sruthi012003@users.noreply.github.com>
2025-11-01 11:45:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,43 +1,25 @@
 provider "aws" {
   region = "us-east-1"
 }
-
+ 
 data "aws_vpc" "existing_vpc" {
   id = "vpc-07e3518f2655e2254"
 }
-
+ 
 resource "aws_subnet" "example_subnet" {
   vpc_id            = data.aws_vpc.existing_vpc.id
   cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-1a"
-
+  availability_zone = "us-east-1a" # updated to match region
+ 
   tags = {
     Name = "Example-Subnet"
   }
 }
-
+ 
 resource "aws_internet_gateway" "gw" {
   vpc_id = data.aws_vpc.existing_vpc.id
-
+ 
   tags = {
     Name = "datasource-Terraform-internet-gateway"
   }
 }
-
-resource "aws_route_table" "example_rt" {
-  vpc_id = data.aws_vpc.existing_vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw.id
-  }
-
-  tags = {
-    Name = "Example-Route-Table"
-  }
-}
-
-resource "aws_route_table_association" "example_rta" {
-  subnet_id      = aws_subnet.example_subnet.id
-  route_table_id = aws_route_table.example_rt.id
-} 
