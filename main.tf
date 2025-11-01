b6f6a140a4e1117eba22d569db2aae851d445ecb
sruthi012003 <121019442+sruthi012003@users.noreply.github.com>
2025-11-01 11:51:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ data "aws_vpc" "existing_vpc" {
  
 resource "aws_subnet" "example_subnet" {
   vpc_id            = data.aws_vpc.existing_vpc.id
-  cidr_block        = "10.0.2.0/24"
+  cidr_block        = "10.0.3.0/24"
   availability_zone = "us-east-1a" # updated to match region
  
   tags = {
@@ -23,4 +23,5 @@ resource "aws_internet_gateway" "gw" {
     Name = "datasource-Terraform-internet-gateway"
   }
 }
+
 
