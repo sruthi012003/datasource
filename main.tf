bfb84c51b2eee44f87a2fc474891e9d138558ae0
sruthi012003 <121019442+sruthi012003@users.noreply.github.com>
2025-11-01 11:18:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,111 +1,43 @@
 provider "aws" {
   region = "us-east-1"
 }
- 
-resource "aws_vpc" "main_vpc" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_support   = true
-  enable_dns_hostnames = true
- 
+
+data "aws_vpc" "existing_vpc" {
+  id = "vpc-07e3518f2655e2254"
+}
+
+resource "aws_subnet" "example_subnet" {
+  vpc_id            = data.aws_vpc.existing_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+
   tags = {
-    Name = "Terraform-VPC"
+    Name = "Example-Subnet"
   }
 }
- 
-resource "aws_subnet" "public_subnet" {
-  vpc_id                  = aws_vpc.main_vpc.id
-  cidr_block              = "10.0.1.0/24"
-  map_public_ip_on_launch = true
-  availability_zone       = "us-east-1a"
- 
-  tags = {
-    Name = "Terraform-public-subnet"
-  }
-}
- 
+
 resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.main_vpc.id
- 
+  vpc_id = data.aws_vpc.existing_vpc.id
+
   tags = {
-    Name = "Terraform-internet-gateway"
+    Name = "datasource-Terraform-internet-gateway"
   }
 }
- 
-resource "aws_route_table" "public_rt" {
-  vpc_id = aws_vpc.main_vpc.id
- 
+
+resource "aws_route_table" "example_rt" {
+  vpc_id = data.aws_vpc.existing_vpc.id
+
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.gw.id
   }
- 
+
   tags = {
-    Name = "Terraform-route-table"
+    Name = "Example-Route-Table"
   }
 }
- 
-resource "aws_route_table_association" "public_assoc" {
-  subnet_id      = aws_subnet.public_subnet.id
-  route_table_id = aws_route_table.public_rt.id
-}
- 
-resource "aws_s3_bucket" "terraform_bucket" {
-  bucket        = "terraform-demo-bucket-assignment2"
-  force_destroy = true
- 
-  tags = {
-    Name        = "Terraform-S3-bucket"
-    Environment = "Dev"
-  }
-}
- 
-provider "aws" {
-  region = "us-east-1"
-}
-resource "aws_vpc" "main_vpc" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_support   = true
-  enable_dns_hostnames = true
-  tags = {
-    Name = "Terraform-VPC"
-  }
-}
-resource "aws_subnet" "public_subnet" {
-  vpc_id                  = aws_vpc.main_vpc.id
-  cidr_block              = "10.0.1.0/24"
-  map_public_ip_on_launch = true
-  availability_zone       = "us-east-1a"
-  tags = {
-    Name = "Terraform-public-subnet"
-  }
-}
-resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.main_vpc.id
-  tags = {
-    Name = "Terraform-internet-gateway"
-  }
-}
-resource "aws_route_table" "public_rt" {
-  vpc_id = aws_vpc.main_vpc.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw.id
-  }
-  tags = {
-    Name = "Terraform-route-table"
-  }
-}
-resource "aws_route_table_association" "public_assoc" {
-  subnet_id      = aws_subnet.public_subnet.id
-  route_table_id = aws_route_table.public_rt.id
-}
-resource "aws_s3_bucket" "terraform_bucket" {
-  bucket        = "terraform-demo-bucket-assignment2"
-  force_destroy = true
-  tags = {
-    Name        = "Terraform-S3-bucket"
-    Environment = "Dev"
-  }
-}
- 
- 
+
+resource "aws_route_table_association" "example_rta" {
+  subnet_id      = aws_subnet.example_subnet.id
+  route_table_id = aws_route_table.example_rt.id
+} 
